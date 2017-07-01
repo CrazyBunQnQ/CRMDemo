b22191b84995f124f91035ba114f5da8c42a159b
CrazyBunQnQ <baobao222222@qq.com>
2017-07-01 21:52:19 +0800

    create popedom files

--- src/main/java/com/controller/PopedomController.java
+++ src/main/java/com/controller/PopedomController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @version 2017/7/1.
@@ -17,5 +18,15 @@ public class PopedomController {
     @Autowired
     private PopedomService popedomService;
 
+    @RequestMapping("getPopedomTreeStr")
+    public ModelAndView getPopedomTreeStr() {
+        ModelAndView modelAndView = new ModelAndView();
 
+        modelAndView.setViewName("/jsp/role/popedomSet");
+        return modelAndView;
+    }
+
+    public void setPopedomService(PopedomService popedomService) {
+        this.popedomService = popedomService;
+    }
 }
--- src/main/java/com/service/impl/PopedomServiceImpl.java
+++ src/main/java/com/service/impl/PopedomServiceImpl.java
@@ -1,8 +1,13 @@
 package com.service.impl;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+
 /**
  * @version 2017/7/1.
  * @auther CrazyBunQnQ
  */
+@Log4j
+@Service
 public class PopedomServiceImpl {
 }
