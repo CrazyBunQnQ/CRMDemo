4fa40278f6da2fe1d5774e4b1858fd04b316f2c2
CrazyBunQnQ <baobao222222@qq.com>
2017-07-02 11:50:50 +0800

    Optimize the authentication code, remove the 0 s and O

--- src/main/java/com/servlet/VerifyCodeServlet.java
+++ src/main/java/com/servlet/VerifyCodeServlet.java
@@ -40,8 +40,8 @@ public class VerifyCodeServlet extends HttpServlet {
     private int fontHeight;
     private int codeY;
     private char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-            'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     public VerifyCodeServlet() {
         super();
